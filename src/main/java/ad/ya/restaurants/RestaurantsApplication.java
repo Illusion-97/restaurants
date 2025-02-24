d510c2b963e65ba6055d6b93dc09109301219705
Yanis <yanis971.60@gmail.com>
2025-02-24 09:42:52 +0100

    Feat : Starting Project

--- src/main/java/ad/ya/restaurants/RestaurantsApplication.java
+++ src/main/java/ad/ya/restaurants/RestaurantsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestaurantsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsApplication.class, args);
+    }
 
 }
