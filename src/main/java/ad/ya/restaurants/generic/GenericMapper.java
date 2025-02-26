92b7ae0c80d7a32dd009ab5c38c0376687effcc6
Yanis <yanis971.60@gmail.com>
2025-02-26 11:03:06 +0100

    Fix : GenericMapper

--- src/main/java/ad/ya/restaurants/generic/GenericMapper.java
+++ src/main/java/ad/ya/restaurants/generic/GenericMapper.java
@@ -1,9 +1,5 @@
 package ad.ya.restaurants.generic;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
-
-@Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface GenericMapper<D extends BaseDto,E extends BaseEntity> {
 
     D toDto(E entity);
